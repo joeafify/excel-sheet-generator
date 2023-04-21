594c2b50fa1068ce5938bc968a87678674a1acc4
Joe Afify <joe.afify95@gmail.com>
2023-04-21 02:39:31 +0200

    Applied alert for validation

--- script.js
+++ script.js
@@ -1,29 +1,66 @@
 let table = document.getElementsByClassName("sheet-body")[0],
-rows = document.getElementsByClassName("rows")[0],
-columns = document.getElementsByClassName("columns")[0]
-tableExists = false
+	rows = document.getElementsByClassName("rows")[0],
+	columns = document.getElementsByClassName("columns")[0],
+	tableExists = false;
 
 const generateTable = () => {
-    let rowsNumber = parseInt(rows.value), columnsNumber = parseInt(columns.value)
-    table.innerHTML = ""
-    for(let i=0; i<rowsNumber; i++){
-        var tableRow = ""
-        for(let j=0; j<columnsNumber; j++){
-            tableRow += `<td contenteditable></td>`
-        }
-        table.innerHTML += tableRow
-    }
-    if(rowsNumber>0 && columnsNumber>0){
-        tableExists = true
-    }
-}
+	let rowsNumber = parseInt(rows.value),
+		columnsNumber = parseInt(columns.value);
+	if (!rowsNumber) {
+		Swal.fire({
+			title: "Error!",
+			text: "Rows field is required",
+			icon: "error",
+		});
+		return;
+	}
+	if (!columnsNumber) {
+		Swal.fire({
+			title: "Error!",
+			text: "Columns field is required",
+			icon: "error",
+		});
+		return;
+	}
+
+	table.innerHTML = "";
+	for (let i = 0; i < rowsNumber; i++) {
+		var tableRow = "";
+		for (let j = 0; j < columnsNumber; j++) {
+			tableRow += `<td contenteditable></td>`;
+		}
+		table.innerHTML += tableRow;
+	}
+	if (rowsNumber > 0 && columnsNumber > 0) {
+		tableExists = true;
+	}
+};
 
 const ExportToExcel = (type, fn, dl) => {
-    if(!tableExists){
-        return
-    }
-    var elt = table
-    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" })
-    return dl ? XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' })
-        : XLSX.writeFile(wb, fn || ('MyNewSheet.' + (type || 'xlsx')))
-}
+	if (!tableExists) {
+		return;
+	}
+	let cells = document.getElementsByTagName("td");
+	let isEmpty = false;
+	for (const cell of cells) {
+		if (!cell.innerText) {
+			isEmpty = true;
+		} else {
+			isEmpty = false;
+			break;
+		}
+	}
+	if (isEmpty) {
+		Swal.fire({
+			title: "Error!",
+			text: "There is no generated table to be exported",
+			icon: "error",
+		});
+		return;
+	}
+	var elt = table;
+	var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" });
+	return dl
+		? XLSX.write(wb, { bookType: type, bookSST: true, type: "base64" })
+		: XLSX.writeFile(wb, fn || "MyNewSheet." + (type || "xlsx"));
+};
